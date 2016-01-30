394d05840609affac9a2181c03e8120f5360fa53
Nick Soffa <nsoffa@lion.lmu.edu>
2016-01-30 14:34:59 -0800

    Started scene and working on Stickman

---
+++ stickman.js
@@ -0,0 +1,17 @@
+(function () {
+	
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+
+	//Head
+	var radius = 20;
+	ctx.arc(210, 200, radius, 0, 2 * Math.PI, false);
+	ctx.fill();
+
+	//Body
+	ctx.fillRect(200, 200, 15, 100);
+
+	//Legs
+	ctx.fillRect(200, 350, 15, 75);
+
+}());
