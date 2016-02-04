63cee8cd94752f785ba8869923b5e3d34f92c844
Nick Soffa <nsoffa@lion.lmu.edu>
2016-02-04 02:11:07 -0800

    Finished Target parameterization, and finished bow parameterization

--- bow.js
+++ bow.js
@@ -2,7 +2,99 @@
 	
 	window.SpriteLibrary = window.SpriteLibrary || { };
 
-	SpriteLibrary.bow = function () {
+	var drawBow = function (ctx, x1, y1, x2, y2, x3, y3, bowLineWidth) {
+		//Top half
+		ctx.save();
+		ctx.beginPath();
+		ctx.strokeStyle = "brown";
+		ctx.moveTo(0, 0);
+		ctx.bezierCurveTo(x1, y1, x2, y2, x3, y3);
+		ctx.lineWidth = bowLineWidth;
+		ctx.stroke();
+		ctx.closePath();
+		ctx.restore();
 
+		// Bottom Half
+	  	ctx.save();
+		ctx.beginPath();
+		ctx.scale(1, -1);
+		ctx.translate(0, -360);
+		ctx.strokeStyle = "brown";
+		ctx.moveTo(0, 0);
+		ctx.bezierCurveTo(x1, y1, x2, y2, x3, y3);
+		ctx.lineWidth = bowLineWidth;
+		ctx.stroke();
+		ctx.closePath();
+		ctx.restore();
+
+		// Handle
+		ctx.fillStyle = "black";
+		ctx.fillRect(x3 - 4, y3 - 10, 10, 40);
+	}
+
+	var drawString = function (ctx, pullPointX, pullPointY) {
+
+		//Top Half
+		ctx.save();
+		ctx.beginPath();
+		ctx.lineWidth = 1;
+		ctx.moveTo(0, 0);
+		ctx.lineTo(pullPointX, pullPointY);
+		ctx.stroke();
+		ctx.closePath();
+		ctx.restore();
+
+		//Bottom Half
+		ctx.save();
+		ctx.beginPath();
+		ctx.scale(1, -1);
+		ctx.translate(0,-360);
+		ctx.moveTo(0, 0);
+		ctx.lineTo(pullPointX, pullPointY);
+		ctx.lineWidth = 1;
+		ctx.stroke();
+		ctx.closePath();
+		ctx.restore();
+	}
+
+	SpriteLibrary.bow = function (bowSpecification) {
+		var ctx = bowSpecification.ctx;
+
+		var x1 = 0;
+		var y1 = 80;
+		var x2 = 100;
+		var y2 = 25;
+		var x3 = 50;
+		var y3 = 170;
+		var bowLineWidth = 10;
+
+		var stringLineWidth = 1;
+		var pullPointX = bowSpecification.pullPointX || 0;
+		var pullPointY = y3 + 10;
+
+		drawBow(ctx, x1, y1, x2, y2, x3, y3, bowLineWidth);
+
+		drawString(ctx, pullPointX, pullPointY);
+
+		//String
+		// ctx.save();
+		// ctx.beginPath();
+		// ctx.lineWidth = 1;
+		// ctx.moveTo(0, 0);
+		// ctx.lineTo(0, pullPoint);
+		// ctx.stroke();
+		// ctx.closePath();
+		// ctx.restore();
+
+		// ctx.save();
+		// ctx.beginPath();
+		// ctx.scale(1, -1);
+		// ctx.translate(0,-360);
+		// ctx.moveTo(0, 0);
+		// ctx.lineTo(0, pullPoint);
+		// ctx.lineWidth = 1;
+		// ctx.stroke();
+		// ctx.closePath();
+		// ctx.restore();
 	};
 }());
--- target.js
+++ target.js
@@ -2,53 +2,81 @@
 	
 	window.SpriteLibrary = window.SpriteLibrary || { };
 
-	SpriteLibrary.target = function (targetSpecification) {
-		var ctx = targetSpecification.ctx;
+	var drawRightLeg = function (ctx, legAngle, opacity) {
 
-		//Drawing Legs
-		var rightStandAngle = -Math.PI / 4;
-		var leftStandAngle = Math.PI / 10;
-
-		//Right Leg
 		ctx.save();
 		ctx.beginPath();
-		ctx.fillStyle = "brown";
-		ctx.rotate(rightStandAngle);
+		ctx.fillStyle = "rgba(165, 42, 42, " + opacity + ")";
+		ctx.rotate(legAngle);
 		ctx.translate(0, 150);
 		ctx.fillRect(0, 0, 15, 125);
 		ctx.closePath();
 		ctx.restore();
 
-		//Left Leg
+	}
+
+	var drawLeftLeg = function (ctx, legAngle, opacity) {
+
 		ctx.save();
 		ctx.beginPath();
-		ctx.fillStyle = "brown";
-		ctx.rotate(leftStandAngle);
+		ctx.fillStyle = "rgba(165, 42, 42, " + opacity + ")";
+		ctx.rotate(legAngle);
 		ctx.translate(145, 80);
 		ctx.fillRect(0, 0, 15, 80);
 		ctx.closePath();
 		ctx.restore();
 
-		//Drawing each circle of the target
+	}
+
+	var drawTarget = function (ctx, radiusX, radiusY, rotation, opacity) {
+
+		//Draws each circle of the target besides the bullseye, outside circle first
 		for(i = 3; i <= 6; i++){
 			ctx.beginPath();
-			ctx.ellipse(100, 100, 80 / i, 250 / i, -Math.PI / 1.05, 0, 2 * Math.PI);
+			ctx.ellipse(100, 100, radiusX / i, radiusY / i, rotation, 0, 2 * Math.PI);
 			if(i % 2 == 0){
 				ctx.fillStyle = "white";
 			} else {
-				ctx.fillStyle = "red";
+				ctx.fillStyle = "rgba(255, 0, 0, " + opacity + ")";
 			}
 			ctx.fill();
+			ctx.strokeStyle = "rgba(0, 0, 0, " + opacity + ")";
 			ctx.stroke();
 			ctx.closePath();
 		}
+	}
 
-		//Drawing Bulls eye
+	var drawBullseye = function(ctx, radiusX, radiusY, rotation, opacity) {
+
 		ctx.beginPath();
-		ctx.ellipse(100, 100, 8, 25, -Math.PI / 1.05, 0, 2 * Math.PI);
-		ctx.fillStyle = "red";
+		ctx.ellipse(100, 100, radiusX / 10, radiusY / 10, rotation, 0, 2 * Math.PI);
+		ctx.fillStyle = "rgba(255, 0, 0, " + opacity + ")";
 		ctx.fill();
+		ctx.strokeStyle = "rgba(0, 0, 0, " + opacity + ")";
+		ctx.stroke();
 		ctx.closePath();
+
+	}
+
+	SpriteLibrary.target = function (targetSpecification) {
+		var ctx = targetSpecification.ctx;
+
+		var opacity = targetSpecification.opacity || 1;
+
+		var rightStandAngle = targetSpecification.rightStandAngle || (-Math.PI / 4);
+		var leftStandAngle = (Math.PI / 10);
+
+
+		var circleRadiusX = 80;
+		var circleRadiusY = 250;
+		var circleRotation = -Math.PI / 1.05;
+
+		drawRightLeg(ctx, rightStandAngle, opacity);
+		drawLeftLeg(ctx, leftStandAngle, opacity);
+
+		drawTarget(ctx, circleRadiusX, circleRadiusY, circleRotation, opacity);
+
+		drawBullseye(ctx, circleRadiusX, circleRadiusY, circleRotation, opacity);
 
 	};
 }());
